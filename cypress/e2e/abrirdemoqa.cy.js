b2bcbf9c7b83494a9d1834760263c3f92e968b8f
Thiago D'avila <101297988+ThiagoDavila@users.noreply.github.com>
2024-03-16 12:34:31 -0300

    att testes

---
+++ cypress/e2e/abrirdemoqa.cy.js
@@ -0,0 +1,19 @@
+describe('Testeando elementos na demoqa', () => {
+  it('abrir o site', () => {
+    cy.visit('https://demoqa.com')
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('.category-cards > :nth-child(1) > :nth-child(1)').click();
+    /* ==== End Cypress Studio ==== */
+  })
+  it('testar o Text Box',() => {
+
+  })
+})
+
+
+
+/*
+it('',() => {
+    
+  })
+*/ 
